4b3d0ea4c8095f898aac006bd304b7330332732d
Catherine Debayle <catherinedebayle@MacBook-Pro.home>
2020-04-01 09:47:41 +0200

    database mongo+mongoose setup

--- server/src/app.js
+++ server/src/app.js
@@ -3,20 +3,33 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
-
-
+const  Config = require('./config/Config')
 const app = express();
+
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose.connect('mongodb+srv://hypertube:' + 
+Config.db.password + 
+'@cluster0-ybiyr.mongodb.net/test?retryWrites=true&w=majority',
+{
+    // useMongoClient: true,
+    useNewUrlParser: true
+});
+
+/// Connect to app routes 
+require('./routes')(app)
+
 
 
 // app.get('/', function (req, res) {
 //     res.send('Hello World!')
 //   })
 
-let port = 8081;
-app.listen(port, () =>  {
-    console.log(`listening server side on ${port} Waiting on launching Mongoose Database`)
+
+
+app.listen(Config.port, () =>  {
+    console.log(`listening server side on ${Config.port} Waiting on launching Mongoose Database`)
 })
 //synching Databse for error before launching Serveer port
-mongoose.connect
-
--- server/src/config/Config.js
+++ server/src/config/Config.js
@@ -1,1 +1,10 @@
 /// Configuration file for the database, ENV, PORT, & ETC..
+
+module.exports = {
+    port: process.env.PORT || 8081,
+    db: {
+        database: process.env.DB_NAME || 'hypertube',
+        user: process.env.DB_USER || 'tracker',
+        password: process.env.DB_PW || 'tracker',
+    },
+}
